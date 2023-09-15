7e6891cd20923802af911a899b6ea144bebb003c
Shirambere Edward <sedward@Shiramberes-MacBook-Pro.local>
2023-09-15 22:08:31 +0300

    add code to use basic class

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,5 +1,12 @@
 import 'package:cli/cli.dart' as cli;
+import 'package:cli/space_craft.dart' as space_craft;
 
 void main(List<String> arguments) {
   print('Hello world: ${cli.calculate()}!');
+
+  var voyager = space_craft.Spacecraft('Voyager I', DateTime(1997,9,5));
+  voyager.describe();
+
+  var voyager3 = space_craft.Spacecraft.unlaunched('Voyager III');
+  voyager3.describe();
 }
